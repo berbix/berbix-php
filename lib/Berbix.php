b57722dfca5996db87d3544a7318ba0c0ea7a257
Eric Levine <eric@berbix.com>
2019-05-02 11:23:46 -0700

    Extract out the http client for easier testability.

--- lib/Berbix.php
+++ lib/Berbix.php
@@ -3,6 +3,37 @@
 namespace Berbix;
 
 
+class HTTPClient {
+  public function makeRequest($method, $url, $headers, $payload=null) {
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    if ($method == 'POST') {
+      curl_setopt($curl, CURLOPT_POST, true);
+    }
+
+    if ($payload != null) {
+      $content = json_encode($payload);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+    }
+
+    $json_response = curl_exec($curl);
+
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    if ($status < 200 || $status >= 300) {
+      throw new \Exception("unexpected status code returned $status, response was $json_response");
+    }
+
+    curl_close($curl);
+
+    return json_decode($json_response, true);
+  }
+}
+
+
 class UserTokens {
   public $refreshToken;
   public $accessToken;
@@ -26,39 +57,38 @@ class UserTokens {
 
 
 class Client {
-  public $clientId;
-  public $clientSecret;
-  public $apiHost;
+  private $clientId;
+  private $clientSecret;
+  private $apiHost;
+  private $httpClient;
 
-  public function __construct($cId=null, $cSecret=null, $host="https://api.berbix.com") {
+  public function __construct($cId=null, $cSecret=null, $host="https://api.berbix.com", $http=null) {
     $this->clientId = $cId;
     $this->clientSecret = $cSecret;
     $this->apiHost = $host;
+    if ($http == null) {
+      $this->httpClient = new HTTPClient();
+    } else {
+      $this->httpClient = $http;
+    }
+
+    if ($this->clientId == null) {
+      throw new \Exception("clientId must be provided");
+    }
+    if ($this->clientSecret == null) {
+      throw new \Exception("clientSecret must be provided");
+    }
   }
 
   private function fetchTokens($path, $payload) {
     $url = $this->apiHost . $path;
-    $content = json_encode($payload);
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-    curl_setopt($curl, CURLOPT_USERPWD, $this->clientId . ":" . $this->clientSecret);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+    $headers = array(
+      "Content-Type: application/json",
+      "Authorization: Basic " . base64_encode($this->clientId . ":" . $this->clientSecret),
+    );
 
-    $json_response = curl_exec($curl);
-
-    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-    if ($status < 200 || $status >= 300) {
-      throw new \Exception("unexpected status code returned $status, response was $json_response");
-    }
-
-    curl_close($curl);
-
-    $result = json_decode($json_response, true);
+    $result = $this->httpClient->makeRequest("POST", $url, $headers, $payload);
 
     return new UserTokens($result['refresh_token'], $result['access_token'], $result['expires_in'] + time());
   }
@@ -103,29 +133,12 @@ class Client {
 
     $url = $this->apiHost . $path;
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    $headers = array(
       'Content-Type: application/json',
-      'Authorization: Token ' . $userTokens->accessToken)
+      'Authorization: Token ' . $userTokens->accessToken,
     );
 
-    if ($method == 'POST') {
-      curl_setopt($curl, CURLOPT_POST, true);
-    }
-
-    $json_response = curl_exec($curl);
-
-    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-    if ($status < 200 || $status >= 300) {
-      throw new \Exception("unexpected status code returned $status, response was $json_response");
-    }
-
-    curl_close($curl);
-
-    return json_decode($json_response, true);
+    return $this->httpClient->makeRequest($method, $url, $headers);
   }
 
   public function fetchUser($userTokens) {
